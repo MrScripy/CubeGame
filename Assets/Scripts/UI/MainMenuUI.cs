eddd585c6b1907b1deb062f3c7df9d8fec846b91
Rina-Scr <muntyanu.kairna@yandex.ru>
2023-05-26 15:25:33 +0300

    main menu & settings

---
+++ Assets/Scripts/UI/MainMenuUI.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuUI : MonoBehaviour
+{
+    [SerializeField] private GameObject mainMenu;
+    [SerializeField] private GameObject levels;
+    [SerializeField] private GameObject settings;
+
+    void Start()
+    {
+        mainMenu.SetActive(true);
+        levels.SetActive(false);
+        settings.SetActive(false);
+    }
+
+    public void OnPlayButtonClick()
+    {
+        mainMenu.SetActive(false);
+        levels.SetActive(true);
+    }
+
+    public void OnLevelButtonClick(int sceneIndex)
+    {
+        SceneManager.LoadScene(sceneIndex);
+    }
+
+    public void OnSettingsButtonClick()
+    {
+        mainMenu.SetActive(false);
+        settings.SetActive(true);
+    }
+
+    public void OnBackButtonClick()
+    {
+        Start();
+    }
+
+    public void OnQuitButtonClick()
+    {
+        Application.Quit();
+    }
+}
---
+++ Assets/Scripts/UI/SettingsMenu.cs
@@ -0,0 +1,53 @@
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class SettingsMenu : MonoBehaviour
+{
+    [SerializeField] private TMP_Dropdown resolutionsDropdown;
+    private Resolution[] resolutions;
+
+
+    private void Start()
+    {
+        resolutions = Screen.resolutions;
+
+        resolutionsDropdown.ClearOptions();
+
+        List<string> options = new List<string>();
+
+        int currentResolutionIndex = 0;
+
+        for (int i = 0; i < resolutions.Length; i++)
+        {
+            string option = resolutions[i].width + " x " + resolutions[i].height;
+            options.Add(option);
+
+            if (resolutions[i].width == Screen.currentResolution.width &&
+                resolutions[i].height == Screen.currentResolution.height)
+            {
+                currentResolutionIndex = i;
+            }
+        }
+        resolutionsDropdown.AddOptions(options);
+        resolutionsDropdown.value = currentResolutionIndex;
+        resolutionsDropdown.RefreshShownValue();
+    }
+
+    
+    public void SetQuality(int qualitIndex)
+    {
+        QualitySettings.SetQualityLevel(qualitIndex);
+    }
+
+    public void SetFullscreen(bool isFull)
+    {
+        Screen.fullScreen = isFull;
+    }
+
+    public void SetResolution(int resolutionIndex)
+    {
+        Resolution resolution = resolutions[resolutionIndex];
+        Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
+    }
+}
